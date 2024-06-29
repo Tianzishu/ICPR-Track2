58d443e9001189dc3abb1bd0c50d4d8efe78c5d0
Tianzishu <iamayuan@163.com>
2024-06-29 22:31:44 +0800

    add resnet18_4
    add resnet18_4

--- model/net.py
+++ model/net.py
@@ -71,6 +71,58 @@ class Res_SimAM_block(nn.Module):
 
         return out
 
+class LightWeightNetwork(nn.Module):
+    def __init__(self, num_classes=1, input_channels=3, block=Res_SimAM_block, num_blocks=[2, 2, 2, 2], nb_filter=[4, 8, 16,32, 64]):
+        super(LightWeightNetwork, self).__init__()
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv0_0 = self._make_layer(block, input_channels, nb_filter[0])
+        self.conv1_0 = self._make_layer(block, nb_filter[0],   nb_filter[1], num_blocks[0])
+        self.conv2_0 = self._make_layer(block, nb_filter[1],   nb_filter[2], num_blocks[1])
+        self.conv3_0 = self._make_layer(block, nb_filter[2],   nb_filter[3], num_blocks[2])
+        # self.conv4_0 = self._make_layer(block, nb_filter[3],   nb_filter[4], num_blocks[3])
+
+        # self.conv3_1 = self._make_layer(block, nb_filter[3] + nb_filter[4], nb_filter[3])
+        # self.conv2_2 = self._make_layer(block, nb_filter[2] + nb_filter[3], nb_filter[2])
+        # self.conv1_3 = self._make_layer(block, nb_filter[1] + nb_filter[2], nb_filter[1])
+        # self.conv0_4 = self._make_layer(block, nb_filter[0] + nb_filter[1], nb_filter[0])
+
+        self.conv2_1 = self._make_layer(block, nb_filter[2] + nb_filter[3], nb_filter[2])
+        self.conv1_2 = self._make_layer(block, nb_filter[1] + nb_filter[2], nb_filter[1])
+        self.conv0_3 = self._make_layer(block, nb_filter[0] + nb_filter[1], nb_filter[0])
+
+        self.final = nn.Conv2d(nb_filter[0], num_classes, kernel_size=1)
+
+    def _make_layer(self, block, input_channels, output_channels, num_blocks=1):
+        layers = []
+        layers.append(block(input_channels, output_channels))
+        for i in range(num_blocks-1):
+            layers.append(block(output_channels, output_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, input):
+        x0_0 = self.conv0_0(input)
+        x1_0 = self.conv1_0(self.pool(x0_0))
+        x2_0 = self.conv2_0(self.pool(x1_0))
+        x3_0 = self.conv3_0(self.pool(x2_0))
+        # x4_0 = self.conv4_0(self.pool(x3_0))
+
+        # x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], 1))
+        # x2_2 = self.conv2_2(torch.cat([x2_0, self.up(x3_1)], 1))
+        # x1_3 = self.conv1_3(torch.cat([x1_0, self.up(x2_2)], 1))
+        # x0_4 = self.conv0_4(torch.cat([x0_0, self.up(x1_3)], 1))
+
+        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0)], 1))
+        x1_2 = self.conv1_2(torch.cat([x1_0, self.up(x2_1)], 1))
+        x0_3 = self.conv0_3(torch.cat([x0_0, self.up(x1_2)], 1))
+
+        # output = self.final(x0_4)
+        output = self.final(x0_3)
+        return output
+
+
 
 class DCANet(nn.Module):
     def __init__(self, num_classes=1, input_channels=1, block=Res_SimAM_block, num_blocks=[3, 4, 6, 3], nb_filter=[16,32, 64, 128, 256], deep_supervision=True, mode='test'):
@@ -203,9 +255,9 @@ class DCANet(nn.Module):
             return output
 
 
-class LightWeightNetwork(nn.Module):
+class DCANet_L(nn.Module):
     def __init__(self, num_classes=1, input_channels=3, block=Res_SimAM_block, num_blocks=[2, 2, 2, 2], nb_filter=[4, 8, 16, 32, 64], deep_supervision=True, mode='test'):
-        super(LightWeightNetwork, self).__init__()
+        super(DCANet_L, self).__init__()
         self.mode = mode
         self.relu = nn.ReLU(inplace=True)
         self.deep_supervision = deep_supervision
